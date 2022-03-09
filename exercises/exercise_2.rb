fe9208da8fdd27e4eb64b574982d76f466b3e5ca
Sarah Melanie White <sawrrawr@gmail.com>
2022-03-08 21:37:53 -0330

    add exercise 2

--- exercises/exercise_2.rb
+++ exercises/exercise_2.rb
@@ -5,3 +5,12 @@ puts "Exercise 2"
 puts "----------"
 
 # Your code goes here ...
+store1 = Store.find_by(id: 1)
+store1.name = "New Burnaby"
+store1.save
+
+store2 = Store.find_by(id: 2)
+
+# puts Store.count
+
+
