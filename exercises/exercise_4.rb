4a1ff245e5621b8c2861c6b0cdd798b6b438e66a
Sarah Melanie White <sawrrawr@gmail.com>
2022-03-13 19:09:57 -0230

    add exercises

--- exercises/exercise_4.rb
+++ exercises/exercise_4.rb
@@ -7,3 +7,31 @@ puts "Exercise 4"
 puts "----------"
 
 # Your code goes here ...
+store4 = Store.create do |u|
+  u.name = "Surrey"
+  u.annual_revenue = 224000
+  u.mens_apparel = false
+  u.womens_apparel = true
+end
+
+store5 = Store.create do |u|
+  u.name = "Whistler"
+  u.annual_revenue = 1900000
+  u.mens_apparel = true
+  u.womens_apparel = false
+end
+
+store6 = Store.create do |u|
+  u.name = "Yaletown"
+  u.annual_revenue = 430000
+  u.mens_apparel = true
+  u.womens_apparel = true
+end
+
+@mens_stores = Store.where(mens_apparel: true)
+
+puts @mens_stores
+
+@womens_stores = Store.where("annual_revenue > ?", 1000000, {womens_apparel: true} )
+
+puts @womens_stores
--- exercises/exercise_6.rb
+++ exercises/exercise_6.rb
@@ -9,3 +9,7 @@ puts "Exercise 6"
 puts "----------"
 
 # Your code goes here ...
+@store1.employees.create(first_name: "Khurram", last_name: "Virani", hourly_rate: 60)
+@store1.employees.create(first_name: "Someone", last_name: "Else", hourly_rate: 40)
+@store2.employees.create(first_name: "Ellie", last_name: "Cat", hourly_rate: 80)
+@store2.employees.create(first_name: "Arlo", last_name: "Cat", hourly_rate: 80)
